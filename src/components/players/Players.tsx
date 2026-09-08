5032c576196616b5e9869729b35b0984375db07a
junayedhasan302 <junayedhasan302@gmail.com>
2026-09-09 03:23:00 +0600

    Button Toggle Added

--- src/components/players/Players.tsx
+++ src/components/players/Players.tsx
@@ -1,6 +1,7 @@
-import { use } from "react";
+import { use, useState } from "react";
 import type { IPlayer } from "../../types/playerType";
 import AvailablePlayers from "./AvailablePlayers";
+import SelectedPlayers from "./SelectedPlayers";
 
 interface PlayersProps {
   playersPromise: Promise<IPlayer[]>;
@@ -8,21 +9,24 @@ interface PlayersProps {
 
 const Players = ({ playersPromise }: PlayersProps) => {
   const players = use(playersPromise); // Actual data in here
-
-  console.log(players);
-
+  // console.log(players);
+  const [ButtonType,setButtonType] = useState("Available");
+  const handleUpdateButtonType=(type)=>{
+    setButtonType(type);
+  }
   return (
     <div className="container mx-auto">
       <div className="flex justify-between gap-4 mb-2">
-        <h2 className="font-bold text-xl">Available Players</h2>
+        <h2 className="font-bold text-xl">{ButtonType === 'Available'? `Available Players` : `Selected PLayers`}</h2>
 
         <div>
-          <button className="btn btn-success">Available</button>
-          <button className="btn">Selected</button>
+          <button onClick={()=>setButtonType("Available")} className={`btn ${ButtonType==='Available' ? 'btn-success' : ''} rounded-r-none`}>Available</button>
+
+          <button onClick={()=>setButtonType("Selected")} className={`btn  ${ButtonType==='Selected' ? 'btn-success' : ''} rounded-l-none`}>Selected</button>
         </div>
       </div>
 
-      <AvailablePlayers players={players} />
+      {ButtonType === 'Available' ? <AvailablePlayers players={players} /> : <SelectedPlayers/>}
     </div>
   );
 };
---
+++ src/components/players/SelectedPlayers.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedPlayers = () => {
+    return (
+        <div>
+            Selected players
+        </div>
+    );
+};
+
+export default SelectedPlayers;
